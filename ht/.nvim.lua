aa03885ff1b944baff622446d9672037cdd5a1fb
Ezra Ramatong <mail.ezra@pm.me>
2025-08-26 15:28:58 +0200

    feat(ht): add cube css structure

--- ht/.nvim.lua
+++ ht/.nvim.lua
@@ -1,2 +1,1 @@
-vim.g.css_variables_files = { "./assets/css/tokens/primitive.css", "./assets/css/tokens/semantic.css" }
-print("CSS variables loaded!")
+vim.g.css_variables_files = { "./assets/css/tokens.css" }
